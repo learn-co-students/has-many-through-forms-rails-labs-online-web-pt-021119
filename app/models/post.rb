2d1a662da961663ff5048e791b32e2d96f2a76d2
luizfper <luizfper@gmail.com>
2019-07-03 21:46:02 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+	accepts_nested_attributes_for :categories
+	accepts_nested_attributes_for :comments, reject_if: :reject_posts
+
+	def reject_posts(attributes)
+		attributes.values.any?(&:blank?)
+  end
+
+	def categories_attributes=(attributes)
+		attributes.values.each do |attribute|
+			category = Category.find_or_create_by(attribute)
+		end
+	end
 
 end
