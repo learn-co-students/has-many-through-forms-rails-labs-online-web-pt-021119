41cd6c42b95e61f85512ac9b9e41919cffc85762
github username <github email address>
2019-07-08 00:47:48 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,10 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories, reject_if: :all_blank
   #accepts_nested_attributes_for :comments
-
+  def category_ids=(category_ids)
+    category_ids.each do |category_id|
+      category = Category.find(category_id)
+      self.categories << category
+    end
+  end
 end
