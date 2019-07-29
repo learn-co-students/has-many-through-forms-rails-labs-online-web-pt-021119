9d3523f855a03b64604114a70832916c9344a8a3
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-07-28 22:26:24 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+
+  # def category_ids=(category_ids)
+  #   category_ids.each do |category_id|
+  #     self.categories << Category.find(category_id) unless category_id.empty?
+  #   end
+  # end
+  #
+  # def user_comments
+  #   self.comments.map {|c| c.user}.uniq
+  # end
 
 
 end
