7c707eea9534487e177eb3cf0d5903f696a7d57b
cavu757 <jose.pehovazdiez@gmail.com>
2019-07-29 02:16:13 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
-    redirect_to comment.post
+    if params[:comment][:user_id].present? && params[:comment][:user_attributes][:username].present?
+      redirect_to post_path(params[:comment][:post_id])
+    else
+      comment = Comment.create(comment_params)
+      redirect_to post_path(comment_params[:post_id])
+    end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    if params[:comment][:user_id].present?
+      params.require(:comment).permit(:content, :post_id, :user_id)
+    elsif params[:comment][:user_attributes][:username].present?
+      params.require(:comment).permit(:content, :post_id, user_attributes:[:username])
+    end
   end
 end
